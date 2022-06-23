51e11455a7b90c56ba1a9fcb68c803bf266d4697
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-06-23 16:53:21 -0300

    Course

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -34,3 +34,7 @@ VALUES (3, 2);
 
 INSERT INTO tb_user_role (user_id, role_id)
 VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_Uri, img_Gray_Uri)
+VALUES ('Bootcamp SPRING', 'https://i.ytimg.com/vi/sTX0UEplF54/maxresdefault.jpg',
+        'https://static.wixstatic.com/media/785e56_8123aaa19c954bbe8234c6ef64af6b65~mv2.png/v1/fill/w_560,h_142,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/faf26b44-a12c-8fdf-5dec-eb0277a21681.png');
